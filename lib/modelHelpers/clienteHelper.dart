4183d2db94a3d510ac8e5ffd1722f2a15aabf785
Eder Mendes <eamaral@psgtecnologiaaplicada.com.br>
2019-04-25 10:38:22 -0400

    CRUD cliente, model servico

--- lib/modelHelpers/clienteHelper.dart
+++ lib/modelHelpers/clienteHelper.dart
@@ -1,5 +1,6 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import './servicoHelper.dart';
 
 final String clienteTable = "clienteTable";
 final String idCol = 'idCol';
@@ -14,7 +15,8 @@ class ClienteHelper {
 
   //banco de dados
   Database _db;
- Future<Database> get db async {
+
+  Future<Database> get db async {
     if (_db != null) {
       return _db;
     } else {
@@ -23,17 +25,75 @@ class ClienteHelper {
     }
   }
 
-  Future<Database>initDb() async {
+  Future<Database> initDb() async {
     final databasePath = await getDatabasesPath();
     final path = join(databasePath, "appsdb.db");
 
-    return await openDatabase(path,
-        version: 1, onCreate: (Database db, int newerVersion) async {
-          await db.execute("CREATE TABLE $clienteTable($idCol INTEGER PRIMARY KEY, $nomeCol TEXT, $telCol TEXT, $imgCol TEXT)");
-        });
+    return await openDatabase(path, version: 1,
+        onCreate: (Database db, int newerVersion) async {
+      await db.execute(
+          "CREATE TABLE $clienteTable($idCol INTEGER PRIMARY KEY, $nomeCol TEXT, $telCol TEXT, $imgCol TEXT)");
+      await db.execute(
+          "CREATE TABLE $servicoTable($idServicoCol INTEGER PRIMARY KEY, $descricaoCol TEXT, $dataCol TEXT, $precoCol TEXT, FOREIGN KEY (clienteId) REFERENCES $clienteTable($idCol))");
+    });
+  }
+
+  //metodos
+  Future<Cliente> salvaCliente(Cliente cliente) async {
+    Database dataBase = await db;
+    cliente.id = await dataBase.insert(clienteTable, cliente.toMap());
+    return cliente;
+  }
+
+  Future<List> obterTodos() async {
+    Database dataBase = await db;
+    List listMap = await dataBase.rawQuery("SELECT * FROM $clienteTable");
+    List<Cliente> listCliente = List();
+    for (Map c in listMap) {
+      listCliente.add(Cliente.fromMap(c));
+    }
+    return listCliente;
+  }
+
+  Future<Cliente> obterClienteUnico(int id) async {
+    Database dataBase = await db;
+    List<Map> maps = await dataBase.query(clienteTable,
+        columns: [idCol, nomeCol, telCol, imgCol],
+        where: "$idCol = ?",
+        whereArgs: [id]);
+    if (maps.length > 0) {
+      return Cliente.fromMap(maps.first);
+    } else {
+      return null;
+    }
+  }
+
+  Future<int> deleteCliente(int id) async {
+    Database dataBase = await db;
+    return await dataBase
+        .delete(clienteTable, where: "$idCol = ?", whereArgs: [id]);
+  }
+
+  Future<int> atualizaCliente(Cliente cliente) async {
+    Database dataBase = await db;
+    return await dataBase.update(clienteTable, cliente.toMap(),
+        where: "$idCol = ?", whereArgs: [cliente.id]);
+  }
+
+  //Trazer numero de contatos
+  Future<int> numeroClientes() async {
+    Database dataBase = await db;
+    return Sqflite.firstIntValue(
+        await dataBase.rawQuery("SELECT COUNT(*) FROM $clienteTable"));
+  }
+
+  Future close() async {
+    Database dataBase = await db;
+    dataBase.close();
   }
 }
 
+// classe do cliente
 class Cliente {
   int id;
   String nome;
---
+++ lib/modelHelpers/servicoHelper.dart
@@ -0,0 +1,33 @@
+final String servicoTable = "servicoTable";
+final String idServicoCol = 'idServicoCol';
+final String descricaoCol = 'descricaoCol';
+final String dataCol = 'dataCol';
+final String precoCol = 'precoCol';
+
+class ServicoHelper {}
+
+class Servico {
+  int idServico;
+  String descricao;
+  String data;
+  String preco;
+
+  Servico.fromMap(Map map) {
+    idServico = map[idServicoCol];
+    descricao = map[descricaoCol];
+    data = map[dataCol];
+    preco = map[precoCol];
+  }
+
+  Map toMap() {
+    Map<String, dynamic> map = {
+      descricao: descricaoCol,
+      dataCol: data,
+      precoCol: preco
+    };
+    if (idServico != null) {
+      map[idServicoCol] = idServico;
+    }
+    return map;
+  }
+}
