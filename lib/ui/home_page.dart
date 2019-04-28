35543f336de3e79341a5270add1fe47302a0b015
Tesla <Tesla@teste.com>
2019-04-28 10:14:54 -0400

    page servico

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:app_servicos/modelHelpers/clienteHelper.dart';
 import 'package:flutter/material.dart';
 
@@ -15,9 +17,9 @@ class _HomePageState extends State<HomePage> {
     // TODO: implement initState
     super.initState();
 
-    helper.obterTodos().then((list){
+    helper.obterTodos().then((list) {
       setState(() {
-       servicos = list; 
+        servicos = list;
       });
     });
   }
@@ -26,20 +28,70 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Serviços"),
-        backgroundColor: Colors.purpleAccent,
-        centerTitle: true
-      ),
-      backgroundColor: Colors.white,
+          title: Text("Serviços"),
+          backgroundColor: Colors.blue,
+          centerTitle: true),
+      backgroundColor: Colors.black,
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () {},
         child: Icon(Icons.add),
-        backgroundColor: Colors.purpleAccent[500],
+        backgroundColor: Colors.blue,
       ),
       body: ListView.builder(
         padding: EdgeInsets.all(10.0),
         itemCount: servicos.length,
-        itemBuilder: (context, index){},
+        itemBuilder: (context, index) {
+          return _servicoCard(context, index);
+        },
+      ),
+    );
+  }
+
+  Widget _servicoCard(context, index) {
+    return GestureDetector(
+      child: Card(
+        color: Colors.grey[700],
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: servicos[index].img != null
+                          ? FileImage(File(servicos[index].img))
+                          : AssetImage("img/img.jpg")),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      servicos[index].nome,
+                      style: TextStyle(
+                          fontSize: 22.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      servicos[index].data,
+                      style: TextStyle(color: Colors.white, fontSize: 17.0),
+                    ),
+                    Text(
+                      servicos[index].preco.toString(),
+                      style: TextStyle(color: Colors.white, fontSize: 17.0),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/ui/servico_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ServicoPage extends StatefulWidget {
+  @override
+  _ServicoPageState createState() => _ServicoPageState();
+}
+
+class _ServicoPageState extends State<ServicoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
